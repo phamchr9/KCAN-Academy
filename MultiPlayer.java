ab39f9232c303be0507354a3e6318e311742803c
phamchr9 <116521052+phamchr9@users.noreply.github.com>
2022-12-06 13:59:08 -0500

    Add files via upload

--- MultiPlayer.java
+++ MultiPlayer.java
@@ -1,37 +1,8 @@
 package boggle;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import java.util.*;
-import javax.swing.Timer;
 
 public class MultiPlayer extends BoggleGame{
-
-    /**
-     * stores the initial amount of time for each players' turns.
-     */
-    final private int ROUND_TIME_LIMIT = 60;
-
-    /**
-     * stores whether the game is paused.
-     */
-    private boolean gameIsPaused;
-
-    /**
-     * stores the task that will execute after the timer delay.
-     */
-    private ActionListener task;
-
-    /**
-     * stores the time remaining (in seconds).
-     */
-    private int timeRemaining;
-
-    /**
-     * stores the timer for players turns.
-     */
-    private Timer timer;
 
     /**
      * stores whose turn it is currently (1 for Player 1; 2 for Player 2).
@@ -52,29 +23,6 @@ public class MultiPlayer extends BoggleGame{
         this.scanner = new Scanner(System.in);
         this.gameStats = gameStats;
         this.currentTurn = "1";  //Player 1 always goes first
-        this.timeRemaining = ROUND_TIME_LIMIT;  //each player's turn is 60 seconds
-
-        //sets the actions that will be performed after the timer delay
-        this.task = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                timeRemaining -= 1;
-
-                if (timeRemaining == 0) {
-                    timer.stop();
-                    timeRemaining = ROUND_TIME_LIMIT;  //resets the timer
-                    if (currentTurn.equals("1")) {
-                        currentTurn = "2";
-                        System.out.println("\nPlayer 1's turn has ended");
-                        System.out.println("Press ENTER to start Player's 2 turn.");
-                    } else if (currentTurn.equals("2")) {
-                        currentTurn = "";
-                        System.out.println("\nPlayer 2's turn has ended");
-                        System.out.println("Press ENTER to end the round.");
-                    }
-                }
-            }
-        };
 
     }
 
@@ -89,8 +37,6 @@ public class MultiPlayer extends BoggleGame{
      */
 
     public void playRound(int size, String letters){
-        this.timer = new Timer(1000, task);  //initializes the timer delay
-        this.gameIsPaused = false;
 
         //step 1. initialize the grid
         this.grid = new BoggleGrid(size);
@@ -145,35 +91,24 @@ public class MultiPlayer extends BoggleGame{
 
     private void player1Move(){
 
-        timer.start();
 
         System.out.println("\nIt's Player 1's turn to find some words!\n");
         String inPut;
 
         while(true) {
             //step 1. Print the board for the user, so they can scan it for words
-            if (!gameIsPaused) {
-                System.out.println(this.grid.toString());
-                System.out.print("Enter a word, press [P] to pause the game: ");
-            }
+            System.out.println(this.grid.toString());
+            System.out.print("Enter a word or press ENTER to end the turn: ");
 
             //step 2. Get a input (a word) from the user via the console
             inPut = scanner.nextLine().toUpperCase();
 
-            if(currentTurn.equals("2")) {
+            if(inPut == "") {
                 break; //end round if user inputs nothing
-            } else if (inPut.equals("P")) {  //pauses the game
-                timer.stop();
-                gameIsPaused = true;
-                System.out.println("Game paused. Press [S] to continue game.");
-            } else if (inPut.equals("S") && gameIsPaused) {  //continues the game
-                timer.start();
-                gameIsPaused = false;
-                System.out.println("Game is resumed.");
             }
 
             //step 3. Check to see if it is valid (note validity checks should be case-insensitive)
-            if (this.allWords.containsKey((inPut.toUpperCase())) && !gameIsPaused) {
+            if (this.allWords.containsKey((inPut.toUpperCase()))) {
                 //step 4. If it's valid, update the player's word list and score (stored in boggleStats)
                 gameStats.addWord(inPut.toUpperCase(), BoggleStats.Player.Player1);
             } else {
@@ -192,35 +127,23 @@ public class MultiPlayer extends BoggleGame{
 
     private void player2Move(){
 
-        timer.start();
-
         System.out.println("\nIt's Player 2's turn to find some words!\n");
         String inPut;
 
         while(true) {
             //step 1. Print the board for the user, so they can scan it for words
-            if (!gameIsPaused) {
-                System.out.println(this.grid.toString());
-                System.out.print("Enter a word, press [P] to pause the game: ");
-            }
+            System.out.println(this.grid.toString());
+            System.out.print("Enter a word or press ENTER to end the turn: ");
 
             //step 2. Get a input (a word) from the user via the console
             inPut = scanner.nextLine().toUpperCase();
 
-            if(currentTurn.equals("")) {
+            if(inPut == "") {
                 break; //end round if user inputs nothing
-            } else if (inPut.equals("P")) {  //pauses the game
-                timer.stop();
-                gameIsPaused = true;
-                System.out.println("Game paused. Press [S] to continue game.");
-            } else if (inPut.equals("S") && gameIsPaused) {  //continues the game
-                timer.start();
-                gameIsPaused = false;
-                System.out.println("Game is resumed.");
             }
 
             //step 3. Check to see if it is valid (note validity checks should be case-insensitive)
-            if (this.allWords.containsKey((inPut.toUpperCase())) && !gameIsPaused) {
+            if (this.allWords.containsKey((inPut.toUpperCase()))) {
                 //step 4. If it's valid, update the player's word list and score (stored in boggleStats)
                 gameStats.addWord(inPut.toUpperCase(), BoggleStats.Player.Player2);
             } else {
