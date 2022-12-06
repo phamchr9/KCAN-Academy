55306a0434c5661724f61491f8dee9505d760022
phamchr9 <christinent.pham@mail.utoronto.ca>
2022-12-06 17:16:49 -0500

    [DEV-1.5] Commit #1

--- Dictionary.java
+++ Dictionary.java
@@ -89,5 +89,5 @@ public class Dictionary {
         return wordList;
     }
 
-    
+
 }
--- MultiPlayer.java
+++ MultiPlayer.java
@@ -1,8 +1,32 @@
 package boggle;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.util.*;
+import javax.swing.Timer;
 
 public class MultiPlayer extends BoggleGame{
+
+    /**
+     * stores the initial amount of time for each players' turns.
+     */
+    final private int ROUND_TIME_LIMIT = 60;
+
+    /**
+     * stores the task that will execute after the timer delay.
+     */
+    private ActionListener task;
+
+    /**
+     * stores the time remaining (in seconds).
+     */
+    private int timeRemaining;
+
+    /**
+     * stores the timer for players turns.
+     */
+    private Timer timer;
 
     /**
      * stores whose turn it is currently (1 for Player 1; 2 for Player 2).
@@ -23,7 +47,29 @@ public class MultiPlayer extends BoggleGame{
         this.scanner = new Scanner(System.in);
         this.gameStats = gameStats;
         this.currentTurn = "1";  //Player 1 always goes first
+        this.timeRemaining = ROUND_TIME_LIMIT;  //each player's turn is 60 seconds
 
+        //sets the actions that will be performed after the timer delay
+        this.task = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                timeRemaining -= 1;
+
+                if (timeRemaining == 0) {
+                    timer.stop();
+                    timeRemaining = ROUND_TIME_LIMIT;  //resets the timer
+                    if (currentTurn.equals("1")) {
+                        currentTurn = "2";
+                        System.out.println("\nPlayer 1's turn has ended");
+                        System.out.println("Press ENTER to start Player's 2 turn.");
+                    } else if (currentTurn.equals("2")) {
+                        currentTurn = "";
+                        System.out.println("\nPlayer 2's turn has ended");
+                        System.out.println("Press ENTER to end the round.");
+                    }
+                }
+            }
+        };
 
     }
 
@@ -38,6 +84,7 @@ public class MultiPlayer extends BoggleGame{
      */
 
     public void playRound(int size, String letters){
+        this.timer = new Timer(1000, task);  //initializes the timer delay
 
         //step 1. initialize the grid
         this.grid = new BoggleGrid(size);
@@ -92,19 +139,21 @@ public class MultiPlayer extends BoggleGame{
 
     private void player1Move(){
 
+        timer.start();
+
         System.out.println("\nIt's Player 1's turn to find some words!\n");
         String inPut;
 
         while(true) {
             //step 1. Print the board for the user, so they can scan it for words
             System.out.println(this.grid.toString());
-            System.out.print("Enter a word or press the Enter key to end turn: ");
+            System.out.print("Enter a word: ");
 
 
             //step 2. Get a input (a word) from the user via the console
             inPut = scanner.nextLine().toUpperCase();
 
-            if(inPut == "") {
+            if(currentTurn.equals("2")) {
                 break; //end round if user inputs nothing
             }
 
@@ -128,19 +177,21 @@ public class MultiPlayer extends BoggleGame{
 
     private void player2Move(){
 
+        timer.start();
+
         System.out.println("\nIt's Player 2's turn to find some words!\n");
         String inPut;
 
         while(true) {
             //step 1. Print the board for the user, so they can scan it for words
             System.out.println(this.grid.toString());
-            System.out.print("Enter a word or press the Enter key to end turn: ");
+            System.out.print("Enter a word: ");
 
 
             //step 2. Get a input (a word) from the user via the console
             inPut = scanner.nextLine().toUpperCase();
 
-            if(inPut == "") {
+            if(currentTurn.equals("")) {
                 break; //end round if user inputs nothing
             }
 
